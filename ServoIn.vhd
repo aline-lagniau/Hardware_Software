beba3c0ffbca54f96ecf1161b7b52db9fa1cf26e
Aline L <100213104+aline-lagniau@users.noreply.github.com>
2022-05-10 17:02:02 +0200

    Add files via upload

---
+++ ServoIn.vhd
@@ -0,0 +1,68 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+
+
+entity ServoIn is
+	port (
+		
+			clk		: in std_logic;
+			rst_n	: in std_logic;
+			ena_in	: in std_logic;
+			reg3		: out std_logic_vector(7 downto 0);
+			reg4		: out std_logic_vector(7 downto 0);
+			gpioin	: in std_logic
+			
+		);
+		
+end entity;
+
+architecture RTL of ServoIn is
+     type state_type is (s0,s1,s2,s3);
+	  signal state : state_type;
+	 
+	 
+begin 
+	process (clk,rst_n,ena_in,gpioin)
+		variable count : natural :=0;
+		constant k : natural :=10e9/20;
+		constant min : natural :=10e6/20;
+	begin
+		if rst_n = '0' then
+			count :=0;
+			state <= s0;
+		elsif rising_edge(clk) then
+			case state is
+				when s0 =>
+					count :=0; -- incrémenter deja à s0 si ok
+					if ena_in = '1' then
+						if gpioin='1'then
+							state <= s1;
+						end if;
+				end if;
+				when s1 =>
+					count := count + 1;
+						if gpioin='0'then
+							reg3 <= std_logic_vector(to_unsigned(count-min,8)); --changer
+							state <= s2;
+						end if;
+				when s2 =>
+				count := count + 1;
+						if gpioin='1'then
+							reg4 <= std_logic_vector(to_unsigned((k/count),8)); --sortir fréquence en fct de clk, pas bon
+							count :=0;
+							if ena_in = '1' then
+								state <=s1;
+							else
+								state <= s0;
+							end if;
+						end if;
+						when others =>
+					count :=0;
+					state <= s0;
+			end case;
+		end if;
+	end process;
+					
+end RTL;
---
+++ ServoIn_TB.vhd
@@ -0,0 +1,88 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+
+
+entity ServoIn_TB is
+
+		
+end entity;
+
+architecture TB of ServoIn_TB is
+   constant PERIOD : time := 20 ns;
+   signal clk: STD_LOGIC :='0';
+	signal rst_n : std_logic := '0';
+	signal sena_in : std_logic := '0';
+	signal sreg3_out	: std_logic_vector(7 downto 0): = "00000000";
+	signal sreg4_out : std_logic_vector(7 downto 0);
+	signal sgpioin : std_logic;
+
+	component ServoIn is
+		port 
+		(
+			clk		: in std_logic;
+			rst_n	: in std_logic;
+			ena_in	: in std_logic;
+			reg3	: out std_logic_vector(7 downto 0);
+			reg4	: out std_logic_vector(7 downto 0);
+			gpioin	: in std_logic;
+
+		);
+	end component;
+begin 
+	
+	dummy_in_I: dummy_in
+	port map (
+		clk			=>clk,
+		rst_n		=>rst_n,
+		ena_in		=>sena_in,
+		reg3		=>sreg3_out,
+		reg4		=>sreg4_out,
+		sgpioin		=>sgpioin
+	);
+
+
+
+	rst_n_P: process
+	begin
+		rst_n <= '0';
+		wait for PERIOD ;
+		rst_n <= '1';
+		wait;
+	end process;
+	clk_P process
+	begin
+		clk <= '0';
+		wait for PERIOD/2;
+		clk <= '1';
+		wait for PERIOD/2;
+	end process;
+
+	stimulus: process
+	begin
+		sgpioin <= '0';
+		if rst_n = '0' then
+			wait until rst_n = '1';
+		end if;
+		for i in 0 to 10 loop	--on choisit n périodes pour avoir 1ms		
+			sena_in <= '1';
+			wait for 1 ms; --tester avec 2ms
+			sena_in <= '0';
+			wait for 20 ms - 1 ms; --tester avec 2ms
+		end for;
+		for i in 0 to 10 loop	--on choisit n périodes pour avoir 1ms				
+			sena_in <= '1';
+			wait for 2 ms; --tester avec 2ms
+			sena_in <= '0';
+			wait for 20 ms - 2 ms; --tester avec 2ms
+		end for;
+		for i in 0 to 10 loop	--on choisit n périodes pour avoir 1ms				
+			sena_in <= '1';
+			wait for (1 ms + (1 ms / 255)); --tester avec 2ms
+			sena_in <= '0';
+			wait for 20 ms - 1 ms + 1 ms / 255; --tester avec 2ms
+		end for;		
+	end progress;
+
+end TB;
